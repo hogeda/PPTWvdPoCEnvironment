5f928ad702e676596c905195400be5be9ba70657
kyohei.uchida <pp1tdr0ku.checat@gmail.com>
2021-03-15 17:15:35 +0900

    Delete testCustomExtension.ps1

--- testCustomExtension.ps1
+++
@@ -1,3 +0,0 @@
-hostname | Out-File -FilePath C:\testCustomExtension.txt -Append
-whoami | Out-File -FilePath C:\testCustomExtension.txt -Append
-get-date | Out-File -FilePath C:\testCustomExtension.txt -Append
